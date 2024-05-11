0c11e2b454a6598fef7cadacb847070c0f1dcc79
Bogdan92 <166215492+Bogdan92@users.noreply.github.com>
2024-05-11 20:21:54 +0300

    Update cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,5 +1,11 @@
 import prompt
 
 def welcome_user(name):
-  name = prompt.string('May I have your name? ')
-  print(name)
+    print(name)
+
+def main():
+    name = prompt.string('May I have your name? ')
+    welcome_user(name)
+
+if __name__ == '__main__':
+    main()
