f620353eddadb1ad72ecaccd3b53aff0e8ff232d
Bogdan92 <166215492+Bogdan92@users.noreply.github.com>
2024-05-12 13:13:21 +0300

    Update brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 
-import brain_games.cli
+from brain_games.cli import welcome_user
+from brain_games.cli import name
 
 
 def greet(text):
@@ -15,4 +16,4 @@ def main():
 if __name__ == '__main__':
     main()
 
-brain_games.cli.welcome_user(name)
+welcome_user(name)
