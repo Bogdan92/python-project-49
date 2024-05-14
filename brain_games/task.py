208a68765354f13e188ba62a0b8a4fcac58543d6
Bogdan92 <166215492+Bogdan92@users.noreply.github.com>
2024-05-14 19:50:00 +0300

    Update task.py

--- brain_games/task.py
+++ brain_games/task.py
@@ -5,6 +5,14 @@ import random
 def random_number():
   print('Question:', random.randint(1, 20))
 
+def user_answer():
+  answer = prompt.string('Your answer: ')
+  if answer == 'yes':
+    print('
+    
+  
+  print(f"{'Hello'}, {name}!")
+
 def even_uneven(number):
   return number % 2 == 0
 
