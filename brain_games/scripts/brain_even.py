e9357d5546919967a6cd11b7fc398f0dedea88ca
Bogdan92 <166215492+Bogdan92@users.noreply.github.com>
2024-05-14 19:14:40 +0300

    Update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -5,8 +5,17 @@ brain-games
 def condition(text):
   print(text)
 
+def number(text):
+  print(text)
+
+def answer(text):
+  print(text)
+
 def main():
   condition('Answer "yes" if the number is even, otherwise answer "no".')
+  number('Question: ')
+
+  answer('Your answer: ')
 
 if __name__ == '__main__':
     main()
