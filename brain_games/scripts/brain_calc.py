865840ddf1bd5a04603d9a1485f5016b1e89baa0
Bogdan92 <166215492+Bogdan92@users.noreply.github.com>
2024-05-17 19:21:07 +0300

    Update brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,45 +1,15 @@
-import prompt
+#!/usr/bin/env python3
 
-import random
+from brain_games.calc import welcome_user
 
-def welcome_user():
-    global name
-    name = prompt.string('May I have your name? ')
-    print(f"{'Hello'}, {name}!")
+from brain_games.calc import user_answer
 
-def condition():
-  print('What is the result of the expression?')
 
-def text():
-  return 'Question:'
 
-def end_game():
-  print(f'Congratulations, {name}!')
+def main():
+  welcome_user()
+  user_answer()
 
-def generate_expression():
-  num1 = random.randint(1, 10)
-  num2 = random.randint(1, 10)
-  operator = random.choice(['+', '-', '*'])
-  expression = f"{num1} {operator} {num2}"
-  return expression
-    
-def generate_expression_with_result():
-  expression = generate_expression()
-  result = eval(expression)
-  return expression, result
 
-def user_answer():
-  condition()
-  for _ in range(3):  
-    expression, result = generate_expression_with_result()
-    print(text(), expression)
-    answer = prompt.string('Your answer: ')
-    if answer == str(result):
-      print('Correct!')
-    elif answer != str(result):
-      print(f"{answer} is wrong answer ;(. Correct answer was {result}.\nLet's try again, {name}!")
-      return
-  end_game()
-
-welcome_user()
-user_answer()
+if __name__ == '__main__':
+    main()
