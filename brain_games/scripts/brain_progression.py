11dedddb064f18c4a8784b78a2d5d41209d4860c
Bogdan92 <166215492+Bogdan92@users.noreply.github.com>
2024-05-26 17:42:38 +0300

    Update brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python3
 
+from brain_games.progression import welcome_user
+
+from brain_games.progression import user_answer
+
+
+def main():
+  welcome_user()
+  user_answer()
+
+
+if __name__ == '__main__':
+    main()
